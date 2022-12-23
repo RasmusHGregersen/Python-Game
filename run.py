557fbff4ae5cd83242df0bce57347ff19fe45072
RasmusHGregersen <106821134+RasmusHGregersen@users.noreply.github.com>
2022-12-23 19:17:24 +0800

    Update run.py

--- run.py
+++ run.py
@@ -282,11 +282,14 @@ class Food(Object):
         super().__init__(pixel_size,borders)
         self.color=(255,0,0)
         
-    def eaten(self):
+    def eaten(self,snake):
         x_pixels=(self.borders[0])/self.pixel_size
         y_pixels=(self.borders[1])/self.pixel_size
         self.x=random.randint(0,x_pixels-1)*self.pixel_size
         self.y=random.randint(0,y_pixels-1)*self.pixel_size
+        while (self.x,self.y) in snake.body:
+            self.x=random.randint(0,x_pixels-1)*self.pixel_size
+            self.y=random.randint(0,y_pixels-1)*self.pixel_size
 
 class SnakeGame():
     bounds= None
