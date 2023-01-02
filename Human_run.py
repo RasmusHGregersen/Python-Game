649eb6b3e5a92951a1d603d654adf63e3efbd629
lucimmm25 <100673443+lucimmm25@users.noreply.github.com>
2023-01-02 14:40:16 +0800

    Delete Human_run.py

--- Human_run.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from SnakeGame import SnakeGame
-from q_table_template import q_table_template
-
-pygame.init()
-game=SnakeGame((720,720),True,1)
-#window=pygame.display.set_mode(game.bounds)
-pygame.display.set_caption("Snake")
-game.main_game(3)
